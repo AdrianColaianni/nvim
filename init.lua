7b7e02d643b18c41548a6b20ea81dfd02ec1d088
Adrian Colaianni <adrian@colaianni.us>
2025-10-01 20:57:07 -0400

    Added shortcut to copy file

--- init.lua
+++ init.lua
@@ -262,3 +262,4 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>v", "<cmd>!xclip -sel c %<CR>", { silent = true })
