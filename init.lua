6dde1e0795af6d3d23b2f2847c3d935b064deb5f
Adrian Colaianni <adrian@colaianni.us>
2026-04-29 14:54:57 -0400

    Added nerdtree

--- init.lua
+++ init.lua
@@ -195,21 +195,13 @@ require('lazy').setup({
   { 'tpope/vim-speeddating' },
   { 'tpope/vim-surround' },
   { 'vimwiki/vimwiki' },
+  {
+    'preservim/nerdtree',
+    config = function()
+      vim.keymap.set("", "<leader>n", ":NERDTreeToggle<cr>")
+    end
+  },
 
-  -- Alternatively, use `config = function() ... end` for full control over the configuration.
-  -- If you prefer to call `setup` explicitly, use:
-  --    {
-  --        'lewis6991/gitsigns.nvim',
-  --        config = function()
-  --            require('gitsigns').setup({
-  --                -- Your gitsigns configuration here
-  --            })
-  --        end,
-  --    }
-  --
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`.
-  --
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
