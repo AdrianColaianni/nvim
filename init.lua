996512cd3bab1a2d424f3a8935103f673b94c256
Adrian Colaianni <adrian@colaianni.us>
2026-05-18 10:47:26 -0400

    Added harpoon

--- init.lua
+++ init.lua
@@ -261,6 +261,20 @@ require('lazy').setup({
       { '<leader>u', '<cmd>UndotreeToggle<cr>' },
     },
   },
+  {
+    'theprimeagen/harpoon',
+    config = function()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+
+      vim.keymap.set("n", "<leader>a", mark.add_file)
+      vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
+
+      for i = 1, 10 do
+        vim.keymap.set("n", string.format("<leader>%s",i%10), function() ui.nav_file(i) end)
+      end
+    end,
+  },
 
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
